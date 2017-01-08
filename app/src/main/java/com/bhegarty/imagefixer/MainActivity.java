4e760f5969ab04d3705a73673a4c9b5fad94bc24
Brendan Hegarty <bhegarty37@gmail.com>
2017-01-08 23:58:06 +0000

    Project setup

---
+++ app/src/main/java/com/bhegarty/imagefixer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bhegarty.imagefixer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    static {
+        System.loadLibrary("opencv_java");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Mat mat = new Mat();
+        Log.d(LOG_TAG, mat.toString());
+    }
+}
