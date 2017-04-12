f7afc5935df3934d11c042ff3265497a203b3c7d
Brendan H <bhegarty37@gmail.com>
2017-04-12 23:41:31 +0100

    Add image bitmap to intent extra & tided up class

--- app/src/main/java/com/bhegarty/imagefixer/MainActivity.java
+++ app/src/main/java/com/bhegarty/imagefixer/MainActivity.java
@@ -8,48 +8,21 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.SurfaceView;
-import android.view.View;
 import android.view.WindowManager;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.imgproc.Imgproc;
-
-public class MainActivity extends Activity /*implements CameraBridgeViewBase.CvCameraViewListener2*/ {
+public class MainActivity extends Activity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static Uri uri;
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-//    private CameraBridgeViewBase mOpenCvCameraView;
 
     static {
         System.loadLibrary("opencv_java3");
     }
-
-//    @Override
-//    public void onResume()
-//    {
-//        super.onResume();
-//        if (isCameraPermissionGranted()) {
-////            mOpenCvCameraView.enableView();
-//            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//            }
-//        }
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,15 +31,11 @@ public class MainActivity extends Activity /*implements CameraBridgeViewBase.CvC
         setContentView(R.layout.activity_main);
 
         if (isCameraPermissionGranted()) {
-//            mOpenCvCameraView.enableView();
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
-//        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.MainCameraView);
-//        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-//        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
@@ -75,48 +44,14 @@ public class MainActivity extends Activity /*implements CameraBridgeViewBase.CvC
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Log.v(LOG_TAG, "Result Okay");
 
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
 
             Intent intent = new Intent(MainActivity.this, ImageActivity.class);
-//            intent.putExtra("image", imageBitmap);
+            intent.putExtra("image", imageBitmap);
             startActivity(intent);
         }
     }
-
-//    @Override
-//    public void onPause()
-//    {
-//        super.onPause();
-////        if (mOpenCvCameraView != null)
-////            mOpenCvCameraView.disableView();
-//    }
-//
-//    public void onDestroy() {
-//        super.onDestroy();
-////        if (mOpenCvCameraView != null)
-////            mOpenCvCameraView.disableView();
-//    }
-//
-//    public void onCameraViewStarted(int width, int height) {
-//    }
-//
-//    public void onCameraViewStopped() {
-//    }
-
-//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        Mat inputImage = inputFrame.rgba();
-////        Mat newImage = mOpenCvCameraView.getRotation()
-//        int height = inputFrame.rgba().height();
-//        int width = inputFrame.rgba().width();
-//
-//        Mat newImage = Imgproc.getRotationMatrix2D(new Point(width/2, height/2), 90, 1);
-//
-//        Log.i(LOG_TAG, "HEIGHT: " + inputFrame.rgba().height());
-//        Log.i(LOG_TAG, "WIDTH: " + inputFrame.rgba().width());
-////        return inputFrame.rgba();
-//        return newImage;
-//    }
 
     public boolean isCameraPermissionGranted() {
         if (Build.VERSION.SDK_INT >= 23) {
